61cf5eb9fea7585a40d01bd2ddc073e8f51ad822
belugasanity <aust.bagley@gmail.com>
2016-09-07 20:01:35 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+// Choose your own adventure game v0.1
+alert("Welcome to a burning office building.");
+var userName = prompt("What's your name?","rob");
+var mission = confirm("Are you ready to begin " + userName + "?");
