875b64b4e56619647402939d1b8c70a4b079fded
belugasanity <aust.bagley@gmail.com>
2016-09-21 19:59:33 -0600

    New code for start of game

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
 // Choose your own adventure game v0.1
-alert("Welcome to a burning office building.");
-var userName = prompt("What's your name?","rob");
-var mission = confirm("Are you ready to begin " + userName + "?");
+function userData(){
+	var name = prompt("Is your name steve, sue, or stanley?");
+	var start = confirm("Are you ready to start the game " + name + "?");
+	if(start){
+		alert("Welcome to the game " + name);
+		var choice = prompt("Where would you like to go? The courthouse? the firestation? or the Hospital?");
+		if(choice == "courthouse"){
+			courthouse();
+		}
+		else if(choice == "firestation"){
+			firestation();
+		}
+		else{
+			hospital();
+		}
+	}
+	else{
+		userData();
+	};
+  function courthouse(){
+    console.log("welcome to the courthouse");
+  }
+  function firestation(){
+    console.log("welcome to the firestation");
+  }
+  function hospital(){
+    console.log("welcome to the hospital");
+  }
+}
+userData();
